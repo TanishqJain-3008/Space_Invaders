83e2865861337a6620757a8d41642260b60ff6e3
Tanishqj-3008 <tanishqj3008@gmail.com>
2026-06-28 22:02:05 +0530

    Second commit

--- first.js
+++ first.js
@@ -1,4 +1,13 @@
-console.log("Welcomme to Our Project: Space Invaders");
-const multiply = (a, b) => {
-    return a * b;
-};
+let num = prompt("Enter your marks: ");
+
+if(80 <= num && num <= 100){
+    console.log("A");
+} else if(70 <= num && num < 80){
+    console.log("B");
+} else if(60 <= num && num < 70){
+    console.log("C");
+}else if(50 <= num && num < 60){
+    console.log("D");
+}else if(0 <= num && num < 50){
+    console.log("F");
+}
